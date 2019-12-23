cf74667eac6833cd16a2ba23c4f1009666b932ba
Liam Smith <liamcs98@gmail.com>
2019-12-23 13:36:25 -0500

    day 2 with correct terms

--- Scripted Learning/Advent of Code/2019/2.py
+++ Scripted Learning/Advent of Code/2019/2.py
@@ -4,24 +4,24 @@ import pprint
 
 
 def main():
-    puzzleIn = fetchInput("2in.txt")
+    intcodeMemory = fetchMemory("2in.txt")
 
     # To do this, before running the program,
-    # replace position 1 with the value 12 and replace position 2 with the value 2.
+    # replace addrition 1 with the value 12 and replace addrition 2 with the value 2.
     # Sure..
 
-    puzzleIn[1] = 12
-    puzzleIn[2] = 2
+    intcodeMemory[1] = 12
+    intcodeMemory[2] = 2
     print(engine([1, 0, 0, 0, 99]))
     print(engine([2, 3, 0, 3, 99]))
     print(engine([2, 4, 4, 5, 99, 0]))
     print(engine([1, 1, 1, 4, 99, 5, 6, 0, 99]))
-    postProcess = engine(puzzleIn)
+    addrtProcess = engine(intcodeMemory)
 
-    return postProcess[0]
+    return addrtProcess[0]
 
 
-def fetchInput(filename):
+def fetchMemory(filename):
     # TODO clean this. One Liner?
     with open(filename) as f:
         program = [int(value) for value in f.read().split(',')]
@@ -30,23 +30,34 @@ def fetchInput(filename):
 
 def engine(intcode: list) -> list:
     terminate = False
-    pos = 0
-    
+    EIPAddr = 0
+    acceptedInstructions = [1, 2, 99]
+
     while terminate != True:
-        if intcode[pos] not in [1,2,99]:
-            print('Unknown opcode {} at {}. Terminating'.format(intcode[pos], pos))
+
+        if intcode[EIPAddr] not in acceptedInstructions:
+            print('Unknown opcode {} at addr {}. Terminating'.format(
+                intcode[EIPAddr], EIPAddr))
+            print('Accepted Instructions {}'.format(acceptedInstructions))
             terminate = True
-        elif intcode[pos] == 1:
-            intcode[intcode[pos + 3]] = intcode[intcode[pos + 1]] + intcode[intcode[pos + 2]]
-            pos += 4
-        elif intcode[pos] == 2:
-            intcode[intcode[pos + 3]] = intcode[intcode[pos + 1]] * intcode[intcode[pos + 2]]
-            pos += 4
-        elif intcode[pos] == 99:
+        elif intcode[EIPAddr] == 1:
+            parA = intcode[EIPAddr + 1]
+            parB = intcode[EIPAddr + 2]
+            intcode[intcode[EIPAddr + 3]] = intcode[parA] + \
+                intcode[parB]
+            EIPAddr += 4
+        elif intcode[EIPAddr] == 2:
+            parA = intcode[EIPAddr + 1]
+            parB = intcode[EIPAddr + 2]
+            intcode[intcode[EIPAddr + 3]] = intcode[parA] * \
+                intcode[parB]
+            EIPAddr += 4
+        elif intcode[EIPAddr] == 99:
             terminate = True
             return intcode
 
     return intcode
+
 
 if __name__ == '__main__':
     print(main())
