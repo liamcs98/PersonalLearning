6cd77c165fa711c71124346a3b9611de954d33c5
Liam Smith <liamcs98@gmail.com>
2019-12-23 16:20:38 -0500

    AOC day 4 pt2 featuring Regex!

--- Scripted Learning/Advent of Code/2019/4.py
+++ Scripted Learning/Advent of Code/2019/4.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2019/day/4
 import re
 
+
 def containsAdjacent(passcode: int) -> bool:
     for i, x in enumerate(str(passcode)):
         try:
@@ -22,9 +23,21 @@ def neverdecrease(passcode: int) -> bool:
     else:
         return True
 
+
+def largerGroupMatchingDigits(passcode: int) -> bool:
+    passcodestr = str(passcode)
+    matches = re.findall('00+|11+|22+|33+|44+|55+|66+|77+|88+|99+', passcodestr)
+    for x in matches:
+    	if len(x) == 2:
+    		return True
+    else:
+    	return False
+
+
 if __name__ == '__main__':
     count = 0
+
     for i in range(256310, 732736):
-        if containsAdjacent(i) and neverdecrease(i):
+        if containsAdjacent(i) and neverdecrease(i) and largerGroupMatchingDigits(i):
             count += 1
     print(count)
